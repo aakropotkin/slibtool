56ed10848dd05d3c489415408bcfffe45255e0a7
Alex Ameen <alex.ameen.tx@gmail.com>
2022-02-23 19:13:38 -0600

    update flake lock

--- flake.nix
+++ flake.nix
@@ -1,14 +1,13 @@
 {
   description = "A surrogate libtool implementation";
-  inputs.nixpkgs.follows = "nix/nixpkgs";
-  outputs = { self, nix, nixpkgs, ... }: {
+  outputs = { self, nixpkgs, ... }: {
 
     overlays.slibtool = import ./overlay.nix;
     overlay = self.overlays.slibtool;
 
     packages.x86_64-linux.slibtool = ( import nixpkgs {
       sys = "x86_64-linux";
-      overlays = [self.overlay nix.overlay];
+      overlays = [self.overlay];
     } ).slibtool;
     defaultPackage.x86_64-linux = self.packages.x86_64-linux.slibtool;
 
